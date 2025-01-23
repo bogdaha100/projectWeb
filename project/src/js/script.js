8d03fcbe7c49ce5d90d632f255ade6f94ef33431
unknown <bogdaha100@mail.ru>
2025-01-23 15:09:04 +0500

    fix something + try js in project

---
+++ project/src/js/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const mapBtn = document.querySelector('#mapBtn'),
+        labBtn = document.querySelector('#labBtn');
+
+    mapBtn.addEventListener('click', () => {
+        window.location.href = '/project/index3.html'
+    })
+    labBtn.addEventListener('click', () => {
+        window.location.href = '/project/index3.html'
+    })
+    const myModal = document.getElementById('myModal')
+    const myInput = document.getElementById('myInput')
+
+    myModal.addEventListener('shown.bs.modal', () => {
+        myInput.focus()
+    })
+});
+
+
